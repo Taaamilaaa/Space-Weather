48f97a3665b21be888a93d6183b41efab5bdb95e
Tamila Yudina <79252896+Taaamilaaa@users.noreply.github.com>
2025-08-04 19:47:49 +0200

    Update nasaAPI.js

--- src/api/nasaAPI.js
+++ src/api/nasaAPI.js
@@ -1,25 +1,22 @@
-import { useEffect } from "react";
 import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_NASA_API_KEY;
 const BASE_URL = "https://api.nasa.gov/DONKI/";
-const GEO_STORM = "GST";
-const SUN_FLARE = "FLR";
-const SOLAR_ENERGETIC_PARTIKLE = "SEP";
-const MAGNETOPAUSE_CROSSING = "MPC";
-const RADIATION_BELT_ENHANCEMENT = "RBE";
-const HIGHT_SPEED_STREAM = "HSS";
 
-const START_DAY = "startDate=2025-07-01"; //yyyy-MM-dd
-const END_DAY = "endDate=2025-07-22"; //yyyy-MM-dd
+export async function fetchData(start, end, eventTypes) {
+  const allResponses = [];
+  if (!start || !end || eventTypes.length === 0) return [];
 
-// useEffect(() => {
-//   async function fetchData() {
-//     const response = await axios.get(
-//       `${BASE_URL}${SUN_FLARE}?${START_DAY}&${END_DAY}&api_key=${API_KEY}`
-//     );
+  for (let event of eventTypes) {
+    try {
+      const response = await axios.get(
+        `${BASE_URL}${event}?startDate=${start}&endDate=${end}&api_key=${API_KEY}`
+      );
+      allResponses.push({ type: event, data: response.data });
+    } catch (error) {
+      console.error(`Error fetching ${event}:`, error);
+    }
+  }
 
-//     console.log(response);
-//   }
-//   fetchData();
-// }, []);
+  return allResponses;
+}
