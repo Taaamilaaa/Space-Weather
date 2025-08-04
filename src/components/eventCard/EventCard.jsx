fd465ce67519d5cc5d127193c9d61dc5fd91a3a9
Tamila Yudina <79252896+Taaamilaaa@users.noreply.github.com>
2025-08-04 19:48:07 +0200

    Update EventCard.jsx

--- src/components/eventCard/EventCard.jsx
+++ src/components/eventCard/EventCard.jsx
@@ -1,27 +1,110 @@
 import styles from "./EventCard.module.css";
 import { Button } from "../button/Button";
-import { EventDataList } from "../eventDataList/EventDataList";
 import CardIcon from "../cardIcon/CardIcon";
 
-function EventCard(props) {
+export function EventCard({
+  type,
+  title,
+  startTime,
+  peakTime,
+  endTime,
+  classType,
+  date,
+  note,
+  instruments,
+  link,
+  linkedEvents,
+  eventTime,
+  kpIndex,
+}) {
+  console.log(kpIndex);
+  console.log(note);
+  console.log(instruments);
+  console.log(linkedEvents);
   return (
-    <div className={styles.card}>
-      <div className={styles.cardTitleWrapper}>
-        <CardIcon />
-        <h3>{props.eventName}</h3>
-      </div>
-      <EventDataList
-        eventName="Solar Flare"
-        datetime="2025-07-28"
-        time="14.00"
-        eventClass="M5"
-      />
+    <li className={styles.cardItem}>
+      <div className={styles.card}>
+        <div className={styles.cardTitleWrapper}>
+          <CardIcon type={type} />
+          <h2>{title}</h2>
+        </div>
+        <ul className={styles.dataList}>
+          <li>
+            <p>
+              Event date:
+              {type === "FLR" ? (
+                <time dateTime={startTime}>{date}</time>
+              ) : (
+                <time dateTime={eventTime}>{date}</time>
+              )}
+            </p>
+          </li>
 
-      <Button type="button" className={styles.cardButton}>
-        More details
-      </Button>
-    </div>
+          {type === "FLR" ? (
+            <>
+              <li>
+                <ul>
+                  <li>
+                    <p>
+                      Event time:
+                      <time dateTime={startTime}>{startTime}</time>
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Start time:
+                      <time dateTime={startTime}>{startTime}</time>
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Peak time:
+                      <time dateTime={peakTime}>{peakTime}</time>
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      End time:
+                      <time dateTime={endTime}>{eventTime}</time>
+                    </p>
+                  </li>
+                </ul>
+              </li>
+              {type === "FLR" && (
+                <li>
+                  <p>
+                    Class:
+                    {classType}
+                  </p>
+                </li>
+              )}
+            </>
+          ) : (
+            <>
+              <li>
+                <p>
+                  Event time:
+                  <time dateTime={eventTime}>{eventTime}</time>
+                </p>
+              </li>
+
+              {type === "GSP" && (
+                <li>
+                  <p>Event kp index: {kpIndex}</p>
+                </li>
+              )}
+            </>
+          )}
+        </ul>
+        <div className={styles.linkWrapper}>
+          <a href={link} target="_blank">
+            <p>Show more information</p>
+          </a>
+          <Button type="button" className={styles.cardButton}>
+            More details
+          </Button>
+        </div>
+      </div>
+    </li>
   );
 }
-
-export default EventCard;
