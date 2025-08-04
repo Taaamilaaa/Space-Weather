c06a2a421f44473a99859c3d5514ff533043221f
Tamila Yudina <79252896+Taaamilaaa@users.noreply.github.com>
2025-08-04 19:47:58 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "modern-normalize";
 import "./styles/reset.css";
 import "./styles/variables.css";
 import "./index.css";
-import App from "./components/app/App.jsx";
+import App from "./app/App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
