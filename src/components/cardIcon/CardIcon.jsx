fa44bc9f67072b3f32137719578b5e908eee218e
Tamila Yudina <79252896+Taaamilaaa@users.noreply.github.com>
2025-08-04 19:48:05 +0200

    Update CardIcon.jsx

--- src/components/cardIcon/CardIcon.jsx
+++ src/components/cardIcon/CardIcon.jsx
@@ -1,10 +1,19 @@
 import { TbSunWind } from "react-icons/tb";
+import { SiApachestorm } from "react-icons/si";
+import { GiSunRadiations } from "react-icons/gi";
+import { GiBarbedSun } from "react-icons/gi";
+import { GiSunSpear } from "react-icons/gi";
 import styles from "./CardIcon.module.css";
 
-function CardIcon() {
+function CardIcon({ type }) {
   return (
     <div className={styles.iconWrapper}>
-      <TbSunWind size={35} color="yellow" />
+      {type === "GST" && <SiApachestorm size={35} />}
+      {type === "FLR" && <TbSunWind size={35} color="yellow" />}
+      {type === "SEP" && <GiSunRadiations size={35} color="yellow" />}
+      {type === "MPC" && <GiBarbedSun size={35} color="yellow" />}
+      {type === "RBE" && <GiSunRadiations size={35} color="yellow" />}
+      {type === "HSS" && <GiSunSpear size={35} color="yellow" />}
     </div>
   );
 }
