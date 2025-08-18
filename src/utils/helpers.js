dc5853ed4e02e1d0aca7b0b1260c7fb4b1ff2cc1
Tamila Yudina <79252896+Taaamilaaa@users.noreply.github.com>
2025-08-18 23:46:23 +0200

    Update helpers.js

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,4 @@
-function eventNameSelect(eType) {
+export function eventNameSelect(eType) {
   const events = {
     GST: "Geo storm",
     FLR: "Sun flare",
@@ -15,7 +15,7 @@ function eventNameSelect(eType) {
 }
 
 export const idGen = () =>
-  Math.floor(Math.random() * (10000 - 5) + 5).toString();
+  Math.floor(Math.random() * (1000000 - 5) + 5).toString();
 
 export function timeCut(date) {
   const time = date.substring(11, 16);
@@ -27,6 +27,48 @@ export function dateCut(date) {
   return day;
 }
 
+export const imgIndexSelect = (arr) => {
+  const min = 0;
+  const max = arr.length;
+  if (max === 0) {
+    return;
+  } else {
+    const index = Math.floor(Math.random() * (max - min) + min);
+
+    return index;
+  }
+};
+
+const elProp = (type, wholeEl, newEl, images) => {
+  const index = imgIndexSelect(images);
+  if (type === "FLR") {
+    newEl.date = dateCut(wholeEl.beginTime);
+    newEl.eventTime = timeCut(wholeEl.beginTime);
+    newEl.startTime = timeCut(wholeEl.beginTime);
+    newEl.peakTime = timeCut(wholeEl.peakTime);
+    newEl.endTime = timeCut(wholeEl.endTime);
+    newEl.classType = wholeEl.classType;
+  } else if (type === "GST") {
+    newEl.eventTime = timeCut(wholeEl.startTime);
+    newEl.kpIndex = wholeEl.allKpIndex[0].kpIndex;
+    newEl.date = dateCut(wholeEl.startTime);
+  } else if (type !== "FLR" && type !== "GST") {
+    newEl.date = dateCut(wholeEl.eventTime);
+    newEl.eventTime = timeCut(wholeEl.eventTime);
+  }
+  newEl.id = idGen();
+  newEl.title = eventNameSelect(type);
+  newEl.link = wholeEl.link;
+  newEl.instruments = wholeEl.instruments
+    ? wholeEl.instruments[0].displayName
+    : "";
+  newEl.note = wholeEl.note ? wholeEl.note : "";
+  newEl.linkedEvents = wholeEl.linkedEvents;
+  newEl.notifications = wholeEl.sentNotifications;
+  newEl.type = type;
+  newEl.img = images[index] ? images[index] : null;
+};
+
 export function dataNormol(data, setEventFlag) {
   const arrResp = [];
 
@@ -34,45 +76,25 @@ export function dataNormol(data, setEventFlag) {
     const type = event.type;
 
     const evData = event.data;
-    if (evData.length === 0) {
-      setEventFlag(true);
-    } else {
-      setEventFlag(false);
-    }
+    const evImages = event.DONKIimg;
+
+    evData.length === 0 ? setEventFlag(true) : setEventFlag(false);
+
     for (const e in evData) {
       if (Object.prototype.hasOwnProperty.call(evData, e)) {
         const element = evData[e];
-
         const el = {};
 
-        el.id = idGen();
-        el.type = type;
-        el.date =
-          type === "FLR"
-            ? dateCut(element.beginTime)
-            : dateCut(element.eventTime);
-        el.title = eventNameSelect(type);
-
-        el.startTime = type === "FLR" ? timeCut(element.beginTime) : "";
-        el.peakTime = type === "FLR" ? timeCut(element.peakTime) : "";
-        el.endTime = type === "FLR" ? timeCut(element.endTime) : "";
-
-        el.eventTime = type !== "FLR" ? timeCut(element.eventTime) : "";
-
-        el.classType = type === "FLR" ? element.classType : "";
-        el.kpIndex = type === "GST" ? element.allKpIndex.kpIndex : "";
-
-        el.link = element.link;
-        el.instruments = element.instruments
-          ? element.instruments[0].displayName
-          : "";
-        el.note = element.note ? element.note : "";
-        el.linkedEvents = element.linkedEvents;
-        el.notifications = element.sentNotifications;
-
+        elProp(type, element, el, evImages);
         arrResp.push(el);
       }
     }
   });
+
   return arrResp;
 }
+export const imgFilter = (arr) => {
+  return arr
+    .map((el) => el.links && el.links[0]?.href)
+    .filter((url) => url && (url.endsWith(".jpg") || url.endsWith(".png")));
+};
