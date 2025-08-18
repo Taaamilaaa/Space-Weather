733a4f04043310234f84316e9e514d84f9245c94
Tamila Yudina <79252896+Taaamilaaa@users.noreply.github.com>
2025-08-18 23:46:47 +0200

    Update nasaAPI.js

--- src/api/nasaAPI.js
+++ src/api/nasaAPI.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { eventNameSelect, imgFilter } from "../utils/helpers.js";
 
 const API_KEY = import.meta.env.VITE_NASA_API_KEY;
 const BASE_URL = "https://api.nasa.gov/DONKI/";
@@ -8,11 +9,23 @@ export async function fetchData(start, end, eventTypes) {
   if (!start || !end || eventTypes.length === 0) return [];
 
   for (let event of eventTypes) {
+    const query = eventNameSelect(event) || event;
+
     try {
       const response = await axios.get(
         `${BASE_URL}${event}?startDate=${start}&endDate=${end}&api_key=${API_KEY}`
       );
-      allResponses.push({ type: event, data: response.data });
+      const eventImages = await axios.get(
+        `https://images-api.nasa.gov/search?q=${query}&media_type=image`
+      );
+
+      const img = imgFilter(eventImages.data.collection.items);
+
+      allResponses.push({
+        type: event,
+        data: response.data,
+        DONKIimg: img,
+      });
     } catch (error) {
       console.error(`Error fetching ${event}:`, error);
     }
