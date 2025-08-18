7f2b4ebf8332eba4790dda630b3c6599dac4993f
Tamila Yudina <79252896+Taaamilaaa@users.noreply.github.com>
2025-08-18 23:46:40 +0200

    Update EventCardList.jsx

--- src/components/eventCardList/EventCardList.jsx
+++ src/components/eventCardList/EventCardList.jsx
@@ -7,24 +7,7 @@ export function EventCardList({ events }) {
         {events.map((e) => {
           for (const el in e) {
             if (Object.prototype.hasOwnProperty.call(e, el)) {
-              return (
-                <EventCard
-                  type={e.type}
-                  title={e.title}
-                  startTime={e.startTime}
-                  peakTime={e.peakTime}
-                  endTime={e.endTime}
-                  eventTime={e.eventTime}
-                  classType={e.classType}
-                  note={e.note}
-                  instruments={e.instruments}
-                  link={e.link}
-                  linkedEvents={e.linkedEvents}
-                  key={e.id}
-                  date={e.date}
-                  kpIndex={e.kpIndex}
-                />
-              );
+              return <EventCard evEl={e} key={e.id} />;
             }
           }
         })}
