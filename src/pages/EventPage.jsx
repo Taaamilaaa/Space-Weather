c8be7b41cd172126ad980c1356fdef0f63e3565e
Tamila Yudina <79252896+Taaamilaaa@users.noreply.github.com>
2025-08-19 22:52:00 +0200

    Update EventPage.jsx

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,6 +1,13 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getSunImage } from "../api/helioviewerAPI";
+import { dateForAPICut } from "../utils/helpers.js";
 
 export function EventPage({ data }) {
+  const [sunImg, setSunImg] = useState("");
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(false);
+
   const { id } = useParams();
   const event = data.find((el) => {
     return el.id === id;
@@ -18,12 +25,41 @@ export function EventPage({ data }) {
     kpIndex,
     img,
   } = event;
+  console.log(loading);
+  useEffect(() => {
+    if (type === "FLR") {
+      const { beginTime } = event;
+      const date = dateForAPICut(beginTime);
+      const loadSunImg = async () => {
+        try {
+          setLoading(true);
+
+          const resp = await getSunImage(date);
+
+          setSunImg(resp);
+        } catch (error) {
+          setError(true);
+        } finally {
+          setLoading(null);
+        }
+      };
+      loadSunImg();
+    }
+  }, []);
 
   return (
     <>
       <h1>{title}</h1>
-      <div>Now showing event with id - {id}</div>;<p>type:{type}</p>
-      <img src={img} alt={title} />
+
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <img
+          src={type === "FLR" ? sunImg : img}
+          alt={title}
+          style={{ width: "500px" }}
+        />
+      )}
       <p>start time:{startTime}</p>
       <p>peak time:{peakTime}</p>
       <p>end time:{endTime}</p>
