436641411c497e2261eec6d2bcd873991c218b62
Tamila Yudina <79252896+Taaamilaaa@users.noreply.github.com>
2025-08-19 22:52:17 +0200

    Update CardIcon.jsx

--- src/components/cardIcon/CardIcon.jsx
+++ src/components/cardIcon/CardIcon.jsx
@@ -3,17 +3,20 @@ import { SiApachestorm } from "react-icons/si";
 import { GiSunRadiations } from "react-icons/gi";
 import { GiBarbedSun } from "react-icons/gi";
 import { GiSunSpear } from "react-icons/gi";
+import { GiRadiations } from "react-icons/gi";
 import styles from "./CardIcon.module.css";
 
 function CardIcon({ type }) {
   return (
     <div className={styles.iconWrapper}>
-      {type === "GST" && <SiApachestorm size={35} />}
-      {type === "FLR" && <TbSunWind size={35} color="yellow" />}
-      {type === "SEP" && <GiSunRadiations size={35} color="yellow" />}
-      {type === "MPC" && <GiBarbedSun size={35} color="yellow" />}
-      {type === "RBE" && <GiSunRadiations size={35} color="yellow" />}
-      {type === "HSS" && <GiSunSpear size={35} color="yellow" />}
+      {type === "GST" && <SiApachestorm size={35} color="rgb(35, 155, 167)" />}
+      {type === "FLR" && <TbSunWind size={35} color="rgb(255, 204, 0)" />}
+      {type === "SEP" && (
+        <GiSunRadiations size={35} color="rgb(225, 170, 54)" />
+      )}
+      {type === "MPC" && <GiBarbedSun size={35} color="rgb(154, 203, 208)" />}
+      {type === "RBE" && <GiRadiations size={35} color="rgb(177, 59, 255)" />}
+      {type === "HSS" && <GiSunSpear size={35} color="rgb(0, 101, 248)" />}
     </div>
   );
 }
