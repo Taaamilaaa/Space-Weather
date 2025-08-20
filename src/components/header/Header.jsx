f596586ef86915894a3a2b5afcd38aef301a9768
Tamila Yudina <79252896+Taaamilaaa@users.noreply.github.com>
2025-08-20 16:13:59 +0200

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
 
 export function Header() {
   return (
-    <nav>
-      <Link to="/">Home</Link> | <Link to="/about">About</Link>
-    </nav>
+    <div className={styles.container}>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      </nav>
+    </div>
   );
 }
