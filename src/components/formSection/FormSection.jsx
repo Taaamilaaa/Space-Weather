a7dfdc19e7b3ef97d193df9babadcb6b36ccfc9f
Tamila Yudina <79252896+Taaamilaaa@users.noreply.github.com>
2025-08-20 16:13:55 +0200

    Update FormSection.jsx

--- src/components/formSection/FormSection.jsx
+++ src/components/formSection/FormSection.jsx
@@ -4,9 +4,9 @@ import styles from "./FormSection.module.css";
 export function FormSection({ todayDate, today, maxPastDate, onFormSubmite }) {
   return (
     <>
-      <div className={styles.section}>
+      <div className={styles.container}>
         <div className={styles.textContainer}>
-          <h1>Today: {todayDate}</h1>
+          <h2>Today: {todayDate}</h2>
           <p>Enter a date range and select events.</p>
         </div>
 
