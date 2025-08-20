f4f72d561d7d45be1a8a1603cc8daf28c909a24b
Tamila Yudina <79252896+Taaamilaaa@users.noreply.github.com>
2025-08-20 16:14:08 +0200

    Update SortOptions.jsx

--- src/components/sortOptions/SortOptions.jsx
+++ src/components/sortOptions/SortOptions.jsx
@@ -1,19 +1,21 @@
+import styles from "./SortOptions.module.css";
+
 export function SortOptions({ onSortChange }) {
   return (
-    <>
-      <label htmlFor="sort-select">sort by:</label>
+    <div className={styles.container}>
+      <label htmlFor="sort-select">SORT BY:</label>
       <select
         name="sortItems"
         id="sort-select"
         onChange={(e) => onSortChange(e.target.value)}
       >
-        <option value="">--Please choose an option--</option>
+        <option value="">--Choose an option--</option>
+        <option value="type">By type</option>
         <option value="dateFromNew">By date, from new to old</option>
         <option value="dateFromOld">By date, from old to new</option>
-        <option value="type">By type</option>
         <option value="powerful">By sun flare power, from powerful</option>
         <option value="weak">By sun flare power, from weak to strong</option>
       </select>
-    </>
+    </div>
   );
 }
