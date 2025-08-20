a94618992bd157c468c56333b017418ab81dfb02
Tamila Yudina <79252896+Taaamilaaa@users.noreply.github.com>
2025-08-20 16:14:15 +0200

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import { FormSection } from "../components/formSection/FormSection";
 import { SortOptions } from "../components/sortOptions/SortOptions";
 import { EventCardList } from "../components/eventCardList/EventCardList";
-
+import { InformSection } from "../components/informSetion/InformSection";
 export function HomePage({
   todayDate,
   onFormSubmite,
@@ -28,20 +28,14 @@ export function HomePage({
         />
       </section>
       <section>
-        {loading && <p>Loading data, please wait...</p>}
-        {data && (
-          <p>
-            From {startD} to {endD} we find {data.length} events.
-          </p>
-        )}
-        {eventsFlag && (
-          <p>
-            There are no events from {startD} to {endD}
-          </p>
-        )}
-        {error && (
-          <p>Whoops, something went wrong! Please try reloading this page!</p>
-        )}
+        <InformSection
+          loading={loading}
+          data={data}
+          startD={startD}
+          endD={endD}
+          eventsFlag={eventsFlag}
+          error={error}
+        />
       </section>
 
       {data && (
